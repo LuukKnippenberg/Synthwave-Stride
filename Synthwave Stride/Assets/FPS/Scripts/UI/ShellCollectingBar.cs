c900d916e27b1cca578597b14b4c81091d3e123e
RickvanHam <428294@student.fontys.nl>
2021-09-22 14:44:02 +0200

    Fixed Merge Conflicts

--- Synthwave Stride/Assets/FPS/Scripts/UI/ShellCollectingBar.cs
+++ Synthwave Stride/Assets/FPS/Scripts/UI/ShellCollectingBar.cs
@@ -9,6 +9,8 @@ public class ShellCollectingBar : MonoBehaviour
     public Image ShellFillImage;
     [Tooltip("All the Shells in the game")]
     ShellPickup[] shells;
+    [Tooltip("The text used for the UI")]
+    public Text shelltext;
     public float shellAmount;
     public float shellsCollected;
 
@@ -17,11 +19,13 @@ public class ShellCollectingBar : MonoBehaviour
     {
         shells = FindObjectsOfType<ShellPickup>();
         shellAmount = shells.Length;
+        shelltext.text = "Shells Collected:" + shellsCollected + "/" + shellAmount;
     }
     // Update is called once per frame
     public void UpdateView()
     {
         shellsCollected++;
         ShellFillImage.fillAmount = shellsCollected / shellAmount;
+        shelltext.text = "Shells Collected:" + shellsCollected + "/" + shellAmount;
     }
 }
