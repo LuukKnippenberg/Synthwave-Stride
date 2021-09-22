4ecd2dac1b9db1f7b2cbc7897166851be17b92ea
RickvanHam <428294@student.fontys.nl>
2021-09-22 11:03:04 +0200

    Added Shell Collectable and Shows UI

---
+++ Synthwave Stride/Assets/FPS/Scripts/Gameplay/ShellPickup.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.FPS.Game;
+using Unity.FPS.Gameplay;
+
+public class ShellPickup : Pickup
+{
+    protected override void Start()
+    {
+        base.Start();
+
+        foreach (Transform t in GetComponentsInChildren<Transform>())
+        {
+            if (t != transform)
+                t.gameObject.layer = 0;
+        }
+    }
+
+    protected override void OnPicked(PlayerCharacterController playerController)
+    {
+        
+    }
+}
